c4e0cb0109943968c3635240ef16c73e3aeaedca
1e10Laptop <sample@bths.edu>
2023-02-01 12:15:50 -0500

    e

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Main {
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[]args){
+        ArrayList<Integer> numList = new ArrayList<Integer>();
+
+
+        numList.add(3);
+
+
+        numList.add(0, 6);
+
+
+        numList.add(7);
+
+        numList.add(2, 8);
+
+
+        numList.set(3, 9);
+
+
+        numList.add(1, 2);
+
+        numList.add(1, numList.size());
+
+        numList.remove(4);
+
+        numList.set(numList.size()-1, 0);
+
+        System.out.println(numList);
+
+
+
+    }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Runner {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Runner {
+    public static void main(String[]args){
+// create an array with the known types
+        Integer[] intsArray = {1, 2, 3, 4, 5};
+
+// use the Arrays.asList utility method to convert the array to a list "inline"
+        ArrayList<Integer> intList = new ArrayList<Integer>(Arrays.asList(intsArray));
+        System.out.println(intList);
+
+        Car car1 = new Car("a", 100);
+        Car car2 = new Car("b", 200);
+        Car car3 = new Car("c", 300);
+        Car[] cars = {car1, car2, car3};
+
+        ArrayList<Car> carList = new ArrayList<Car>(Arrays.asList(cars));
+        System.out.println(carList);
+    }
 }
